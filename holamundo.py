f242e943b4fd9a4ca9c6a3314c22c57c5740f4f3
AbrahamGarcia22 <A01023256@itesm.mx>
2020-04-18 18:46:56 -0500

    hola

--- holamundo.py
+++ holamundo.py
@@ -1,3 +1,10 @@
-print("hola")
-def fun(a):
-    print (a)
+import random
+res = input("Â¿desea lanzar los dados?: SI/NO ")
+while( res == 'SI' or 'si'):
+    dado1 = random.choice([1,2,3,4,5,6])
+    dado2 = random.choice([1,2,3,4,5,6])
+
+    print("El dado uno ha caido en: " + (dado1) + " y el dado dos ha caido en: " + (dado2))
+    print("La suma de los dos dados lanzados es: " + str(dado1 + dado2) + "\n")
+
+    res = input("desea lanzar los dados nuevamente?: SI/NO")
